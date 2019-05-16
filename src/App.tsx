4290f484965b92e0febf6446fef25597c1ff9dfa
wlianfu <wlianfu@163.com>
2019-05-16 11:27:29 +0800

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div>
+        <p>Hello, Zeit</p>
+      </div>
+    );
+  }
+}
+
+export default App;
